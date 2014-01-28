20b1b58a6d737ce4a29f6adcb841d34e328b295c
Tommy Fisher <tommy@carbonfive.com>
2014-01-27 17:25:14 -0800

    WIP animations

--- app/scripts/controllers/resource.js
+++ app/scripts/controllers/resource.js
@@ -1,11 +1,14 @@
 (function() {
   'use strict';
 
-  var controller = function($scope, DataStore) {
+  var controller = function($scope, DataStore, $element) {
     $scope.resourceView = this;
     this.resource = $scope.resource;
     this.DataStore = DataStore;
     this.expanded = this.DataStore.get(this.resourceKey());
+
+    this.el = $element;
+    this.scope = $scope;
   };
 
   controller.prototype.resourceKey = function() {
@@ -21,7 +24,28 @@
   };
 
   controller.prototype.expandMethod = function(method) {
+    // var maxHeight = this.el[0].offsetHeight;
+    // var paddingTop = this.el[0].offsetTop;
+    // console.log(this.el, maxHeight, paddingTop);
+
+    // var el = this.el;
+
+    // el.css('max-height', maxHeight + 'px');
+    // el.css('padding-top', paddingTop + 'px');
+    // el.css('margin-top', '-' + paddingTop + 'px');
+
+    // setTimeout(function() {
+    //   el.css('max-height', null);
+    //   el.css('padding-top', null);
+    //   el.css('margin-top', null);
+    // }, 100);
+
     this.methodToExpand = method.method;
+    this.scope.method = method;
+  };
+
+  controller.prototype.collapseMethod = function() {
+    this.scope.method = undefined;
   };
 
   controller.prototype.toggleExpansion = function() {
